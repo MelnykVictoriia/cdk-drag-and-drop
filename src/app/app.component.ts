93933d505e5129295e81b35631e8d3b0f31cdbe6
Melnyk Victoria <melnykvictoria21@gmail.com>
2020-12-11 09:09:21 +0200

    version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { moveItemInArray, CdkDragDrop, transferArrayItem, CdkDragEnter } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cdk-drag-and-drop';
+  items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+  entered(e: CdkDragEnter) {
+    console.log(e);
+    moveItemInArray(this.items, e.item.data, e.container.data);
+  }
+
+  dropped(event: CdkDragDrop<string[]>) {
+
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data, event.previousIndex, 
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
